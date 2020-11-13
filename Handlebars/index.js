023275ecc3e47d9209fd16f79a6bb3fc6bb89675
Tompa02 <30865820+Tompa02@users.noreply.github.com>
2020-11-13 22:31:57 +0100

    fine tutorial

--- Handlebars/index.js
+++ Handlebars/index.js
@@ -7,16 +7,43 @@ const port = 3000;
 app.use(express.static('public'))
 
 
-app.set('view engine', 'handlebars');
-    app.engine('handlebars', handlebars({
+app.set('view engine', 'hbs');
+    app.engine('hbs', handlebars({
         layoutsDir: __dirname + '/views/layouts',
+        extname: 'hbs',
+        defaultLayout: 'planB',
+        partialsDir: __dirname + '/views/partials/'
 }));
 
-
+fakeApi = () => {
+    return [
+      {
+        name: 'Katarina',
+        lane: 'midlaner'
+      },
+      {
+        name: 'Jayce',
+        lane: 'toplaner'
+      },
+      {
+        name: 'Heimerdinger',
+        lane: 'toplaner'
+      },
+      {
+        name: 'Zed',
+        lane: 'midlaner'
+      },
+      {
+        name: 'Azir',
+        lane: 'midlaner'
+      }
+    ];
+}
 
 app.get('/', (req, res) => {
-    res.render('main', {layout : 'index'});
+    res.render('main', {layout: 'index', suggestedChamps: fakeApi(), listExists: true} );
 });
-    
+
+
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
