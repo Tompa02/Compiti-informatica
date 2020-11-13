beb8028d37ffacb87d146245416d690a205663ac
Tompa02 <30865820+Tompa02@users.noreply.github.com>
2020-11-13 09:45:13 +0100

    compito 14/11/20

---
+++ Handlebars/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('public'))
+
+
+app.set('view engine', 'handlebars');
+    app.engine('handlebars', handlebars({
+        layoutsDir: __dirname + '/views/layouts',
+}));
+
+
+
+app.get('/', (req, res) => {
+    res.render('main', {layout : 'index'});
+});
+    
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
